1e4510cdbbddf2fee14a0d0c50ef6d28c59f54ea
Luis-Mocha <luismocha97@gmail.com>
2023-06-29 10:08:09 +0200

    add nav

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -31,5 +31,73 @@
 </template>
 
 <style lang="scss" scoped>
+    nav {
+    height: 90px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-family: 'Cormorant Garamond', serif;
+
+    .nav-logo {
+    text-transform: uppercase;
+    margin-right: 25px;
+    line-height: 1;
+    font-size: 30px;
+    font-weight: 800;
+    }
+
+    ul {
+    width: 70%;
+    display: flex;
+    // justify-content: space-between;
+    flex-wrap: wrap;
+    margin: 0;
+    padding: 0;
+        .nav-li {
+            text-transform: uppercase;
+            position: relative;
+            margin-right: 40px;
+
+            a {
+                padding: 5px 0;
+                display: flex;
+                align-items: center;
+                flex-direction: column;
+                justify-content: space-between;
+                font-weight: 800;
+            }
+        }
+    }
+
+}
+
+/* Effetti hover di .nav-li */
+.nav-li::before, .nav-li::after {
+    content: '';
+    display: block;
+    position: absolute;
+    height: 2px;
+    width: 0;
+    transition: 0.8s;
+}
+.nav-li::before {
+    right: 0;
+    background: linear-gradient(90deg,#7eb09b 0%, #385f50 80%);
+}
+.nav-li::after {
+    content: '';
+    display: block;
+    background: linear-gradient(270deg,#7eb09b 0%, #385f50 80%);
+}
+.nav-li:hover::before, .nav-li:hover::after {
+    width: 100%;
+}
+
+.router-link-active::after {
+    // text-decoration: double;
+    content: '>';
+    position: absolute;
+    left: -12px;
+}
 
 </style>
