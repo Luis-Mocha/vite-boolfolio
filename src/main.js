84dcd6e752242f3c02d4d4c70abe8b4d40759795
Luis-Mocha <luismocha97@gmail.com>
2023-06-29 10:19:55 +0200

    utilizzo parentesi graffe

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 
-import router from './router/router'
+import { router } from './router/router'
 
 //importo bootstrap js
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
