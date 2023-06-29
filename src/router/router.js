bf98ca66ad6f8477ce836260310c13e83edb6b17
Luis-Mocha <luismocha97@gmail.com>
2023-06-29 10:15:03 +0200

    nuova sintassi

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,34 @@
 import { createRouter , createWebHistory} from 'vue-router'
 
-const routes = [
-    {path: '', name: 'home', component: ()=> import('../pages/AppHome.vue')},
-    {path: '/Projects', name: 'projects', component: ()=> import('../pages/AppMain.vue')},
+// const routes = [
+//     {path: '', name: 'home', component: ()=> import('../pages/AppHome.vue')},
+//     {path: '/Projects', name: 'projects', component: ()=> import('../pages/AppMain.vue')},
+// ]
+
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes,
+// })
 
 
-]
+// nuova sinstassi
+import AppHome from '../pages/AppHome.vue';
+import AppMain from '../pages/AppMain.vue';
 
 const router = createRouter({
-    routes,
     history: createWebHistory(),
+    routes: [
+        {
+            path: '',
+            name: 'home',
+            component: AppHome
+        },
+        {
+            path: '/Projects',
+            name: 'projects',
+            component: AppMain
+        },
+    ]
 })
 
 export default router
