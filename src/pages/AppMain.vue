156f34cdbd11aecb1b53a7c2311a6119835f83bf
Luis-Mocha <luismocha97@gmail.com>
2023-06-30 12:21:37 +0200

    function index request

--- src/pages/AppMain.vue
+++ src/pages/AppMain.vue
@@ -11,7 +11,9 @@
             return {
                 projects: [],
                 baseUrlApi: 'http://127.0.0.1:8000/api/',
-                baseUrlStorage: 'http://127.0.0.1:8000/storage/'
+                // baseUrlStorage: 'http://127.0.0.1:8000/storage/',
+                types: null,
+                selectedType: null,
 
             }
         },
@@ -20,20 +22,39 @@
         mounted() {
             // richiamo i projects ogni volta che carico la pagina
             this.getProjects(1);
+            this.getTypes();
         },
         methods: {
 
             getProjects(projectApiPage) {
-                axios.get(`${this.baseUrlApi}projects`, {
-                    params: {
-                        page: projectApiPage
-                    }
-                })
+
+                const params = {
+                    page: projectApiPage
+                }
+
+                if(this.selectedType !== 'all') {
+                    params.type_id = this.selectedType
+                }
+
+                axios.get(`${this.baseUrlApi}projects`, { params })
                 .then(res => {
-                    // console.log(res.data.projects);
+                    console.log(res.data.projects);
                     this.projects = res.data.projects
                 })
             },
+
+            getTypes() {
+                axios.get(`${this.baseUrlApi}types`)
+                .then(res => {
+                    // console.log(res.data);
+                    this.types = res.data.types;
+                })
+            },
+
+            typeFilter() {
+                this.selectedType = document.getElementById("type-filter").value;
+                this.getProjects(1);
+            }
         }
     }
     
@@ -60,7 +81,7 @@
                     <i class="fa-solid fa-caret-left"></i>
                 </button>
 
-                <li v-for="(elem, index) in projects.last_page" :key="index" :class="(projects.current_page === elem) ?'active' : '' ">
+                <li v-for="(elem, index) in projects.last_page" :key="index" :class="(projects.current_page === elem) ? 'active' : '' ">
                     <button @click="getProjects(elem)">
                         {{ elem }}
                     </button>
@@ -75,6 +96,14 @@
                     <i class="fa-solid fa-forward"></i>
                 </button>
             </ul>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Type filter:</label>
+                <select class="form-select form-select-lg" name="" id="type-filter" @change="typeFilter()">
+                    <option value="all" selected>-- All --</option>
+                    <option v-for="(elem, index) in types" :key="index" :value="elem.id"> {{ elem.name }}</option>
+                </select>
+            </div>
 
             <CardProject v-for="(elem, index) in projects.data" :key="index" :infoProject="elem"/>
             
