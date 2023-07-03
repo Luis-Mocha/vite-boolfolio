fd1e503db77846255ed5ac6236fac2f6fcf11bff
Luis-Mocha <luismocha97@gmail.com>
2023-07-03 10:35:26 +0200

    frontend filter for technologies

--- src/pages/AppMain.vue
+++ src/pages/AppMain.vue
@@ -14,6 +14,8 @@
                 // baseUrlStorage: 'http://127.0.0.1:8000/storage/',
                 types: null,
                 selectedType: null,
+                technologies: null,
+                selectedTech: []
 
             }
         },
@@ -23,6 +25,13 @@
             // richiamo i projects ogni volta che carico la pagina
             this.getProjects(1);
             this.getTypes();
+            this.getTechnologies();
+        },
+        watch: {
+            selectedTech: {
+                handler: 'getProjects',
+                deep: true
+            }
         },
         methods: {
 
@@ -32,8 +41,13 @@
                     page: projectApiPage
                 }
 
+                // type
                 if(this.selectedType !== 'all') {
                     params.type_id = this.selectedType
+                }
+                // techs
+                if(this.selectedTech.length > 0) {
+                    params.technologies_ids = this.selectedTech.join(',')
                 }
 
                 axios.get(`${this.baseUrlApi}projects`, { params })
@@ -48,6 +62,14 @@
                 .then(res => {
                     // console.log(res.data);
                     this.types = res.data.types;
+                })
+            },
+
+            getTechnologies() {
+                axios.get(`${this.baseUrlApi}technologies`)
+                .then(res => {
+                    // console.log(res.data);
+                    this.technologies = res.data.technologies;
                 })
             },
 
@@ -67,19 +89,35 @@
     <div class="container pb-5">
 
         <div class="mb-4 filter-section p-3">
-            <label for="" class="form-label">Type filter:</label>
-            <select class="form-select form-select-lg" name="" id="type-filter" @change="typeFilter()">
-                <option value="all" selected>-- All --</option>
-                <option v-for="(elem, index) in types" :key="index" :value="elem.id"> {{ elem.name }}</option>
-            </select>
+            <!-- type -->
+            <div class="mb-3">
+                <label for="" class="form-label">Type filter:</label>
+                <select class="form-select form-select-lg" name="" id="type-filter" @change="typeFilter()">
+                    <option value="all" selected>-- All --</option>
+                    <option v-for="(elem, index) in types" :key="index" :value="elem.id"> {{ elem.name }}</option>
+                </select>
+            </div>
+
+            <!-- technologies -->
+            <div class="d-flex flex-wrap">
+                <span class="d-block w-100">Technologies filter:</span>
+                <div v-for="(elem, index) in technologies" :key="index" class="me-4">
+                    <label :for="`input-${elem.id}`" >
+                        {{ elem.name }}
+                    </label>
+                    <input type="checkbox" :name="elem.id" :value="elem.id" :id="`input-${elem.id}`" v-model="selectedTech" class="ms-1">
+                </div>
+            </div>
+            
+
         </div>
 
         <div class="d-flex justify-content-between">
             <span v-if="this.projects.total === 1">
-                C'è solo "1" progetto con questa tipologia
+                La tua ricerca corrisponde a "1" progetto
             </span>
-            <span v-else-if="this.projects.total > 1">Ci sono "{{ this.projects.total }}" progetti con questa tipologia</span>
-            <span v-else> Non sono presenti progetti con questa tipologia</span>
+            <span v-else-if="this.projects.total > 1">La tua ricerca corrisponde a "{{ this.projects.total }}" progetti</span>
+            <span v-else>La tua ricerca non corrisponde a nessun progetto</span>
 
              <!-- paginazione -->
             <div class="d-flex pagination justify-content-center" v-if="this.projects.last_page > 1">
